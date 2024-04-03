e041033a2dd95a4c625124b8b517827c6c69f4c4
Tailus Dev <4932abcd@gmail.com>
2024-04-03 21:04:54 +0330

    requirement/feature, routing between pages designed with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import GlobalStyled from "./styles/GlobalStyled"
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyled from "./styles/GlobalStyled";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <div>
       <GlobalStyled />
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
