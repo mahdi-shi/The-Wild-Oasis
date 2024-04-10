d5f384159e05a33c99014e900ff465b61665338d
Tailus Dev <4932abcd@gmail.com>
2024-04-10 15:36:49 +0330

    requirement/technology, React query added to the project for managing remote state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import GlobalStyled from "./styles/GlobalStyled";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
@@ -10,9 +13,17 @@ import Settings from "./pages/Settings";
 import PageNotFound from "./pages/PageNotFound";
 import Layout from "./ui/Layout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      stateTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyled />
       <BrowserRouter>
         <Routes>
@@ -28,8 +39,9 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </BrowserRouter>{" "}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
