66448dabe05f3edec00701a29f9924896bcd1df6
Tailus Dev <4932abcd@gmail.com>
2024-06-04 13:18:46 +0330

    ui/feature : profile menu in Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,18 +1,25 @@
-import styled from "styled-components"
-import Logout from "../features/authentication/Logout"
+import styled from "styled-components";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
-    background-color: var(--color-grey-0);
-    padding: 1.2rem 4.8rem;
-    border-bottom: 2px solid var(--color-grey-100);
-`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 2px solid var(--color-grey-100);
+
+  display: flex;
+  justify-content: space-between;
+  gap: 2.4rem;
+  align-items: center;
+`;
 
 function Header() {
-    return (
-        <StyledHeader>
-            <Logout />
-        </StyledHeader>
-    )
+  return (
+    <StyledHeader>
+      <UserAvatar />
+      <HeaderMenu />
+    </StyledHeader>
+  );
 }
 
-export default Header
+export default Header;
