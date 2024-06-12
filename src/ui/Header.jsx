5b4f81d9d3750db1d8fd1b2118645fa2da33b706
Tailus Dev <4932abcd@gmail.com>
2024-06-12 16:09:02 +0330

    testing

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,13 +4,12 @@ import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-100);
-  padding: 1.2rem 4.8rem;
+  padding: 3.2rem 4.8rem;
   border-bottom: 2px solid var(--color-grey-100);
   display: flex;
   justify-content: space-between;
   gap: 2.7rem;
   align-items: center;
-  margin-top: 30px;
 `;
 
 function Header() {
